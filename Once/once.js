7764435094511ba52bbeed6209a52f1d862147b0
annvbur <Ann@WaverleyQAs-Mac-mini-4496.local>
2015-05-09 19:04:47 +0300

    - “once” function

---
+++ Once/once.js
@@ -0,0 +1,39 @@
+var sourceFn = function () {
+	console.log("[SourceFn]: Hey, I’ve been called");
+}
+
+
+
+function once(){
+	var sourceArgs = arguments;
+
+return function(){
+	if(!arguments.callee.once){
+	if (typeof sourceArgs[0] === 'function'){
+		sourceArgs[0].apply(this, arguments);
+	}
+		arguments.callee.once = true;
+};	
+}
+}
+
+
+
+
+var sourceFnOnce = once(sourceFn);
+
+sourceFnOnce();
+sourceFnOnce();
+sourceFnOnce();
+sourceFnOnce();
+sourceFnOnce();
+sourceFnOnce();
+sourceFnOnce();
+sourceFnOnce();
+sourceFnOnce();
+sourceFnOnce();
+sourceFnOnce();
+sourceFnOnce();
+
+
+
