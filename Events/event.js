b20ee24e0e703c52fc4f847142e3f2373cd23d69
annvbur <Ann@WaverleyQAs-Mac-mini-4496.local>
2015-06-17 15:24:57 +0300

    small fix

--- Events/event.js
+++ Events/event.js
@@ -1,15 +1,11 @@
 function drag(elementToDrag, event){
-
-	var startX = event.clientX; //координаты указателя мыши, когда она кликнута по горизонтали
+	var startX = event.clientX; 
+	 //координаты указателя мыши, когда она кликнута по горизонтали
 	var startY = event.clientY; // по вертикали
-
-    var origX = elementToDrag.offsetLeft; // первоначальные координаты элемента, который будет перемещаться: отступ слева
+	var origX = elementToDrag.offsetLeft; // первоначальные координаты элемента, который будет перемещаться: отступ слева
     var origY = elementToDrag.offsetTop; // отступ сверху (в пикселях)
-
-    var deltaX = startX - origX; // расстояние между mousedown  и верхним левым углом элемента
+	var deltaX = startX - origX; // расстояние между mousedown  и верхним левым углом элемента
     var deltaY = startY - origY;
-
- 
 
     document.addEventListener("mousemove", moveHandler, true);
     document.addEventListener("mouseup", upHandler, true);
@@ -17,12 +13,14 @@ function drag(elementToDrag, event){
     //event.stopPropagation(); // откл. дальнейшее распространение события
     //event.preventDefault(); // откл. действий по умолчанию
 
-
     function moveHandler(event) 
     { 
     // перемещаем элемент к текущей позиции курсора 
-    elementToDrag.style.left = (event.clientX - deltaX) +'px'; 
-    elementToDrag.style.top = (event.clientY - deltaY) +'px'; 
+    var newXPosition = event.clientX - deltaX;
+    elementToDrag.style.left = (newXPosition<0?0:newXPosition) +'px';
+
+    var newYPosition = event.clientY - deltaY; 
+    elementToDrag.style.top = (newYPosition<0?0:newYPosition) +'px'; 
     event.stopPropagation(); 
     } 
 
